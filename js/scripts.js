d938ec3a8512440470cccb102fbe12442a6584ec
gracie lundell and taylor grohs <taylorgrohs@gmail.com>
2016-01-21 10:46:02 -0800

    Add UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-var count = function(num) {
-  var num = 30;
-    return true;
+var count = function(countBy, countTo) {
+  var array = []
+  for (var i = 0; i <= countTo; i += countBy) {
+    array.push(" " + i);
+  }
+  return array;
 };
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+  var countBy = parseInt($("input#countBy").val());
+  var countTo = parseInt($("input#countTo").val());
+  $(".result").text(count(countBy, countTo));
+
+  event.preventDefault();
+  });
+});
--- specs/spec.js
+++ specs/spec.js
@@ -1,5 +1,5 @@
 describe("count", function() {
-  it("will count to 30", function() {
-    expect(count(30)).to.equal(true);
+  it("will count up to 30", function() {
+    expect(count(5, 30)).to.eql([0, 5, 10, 15, 20, 25, 30]);
   });
 });
